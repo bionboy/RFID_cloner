860c97cd661cfbac551fd3ae3894f88cf87a515b
Luke Floden <luke.floden@gmail.com>
2018-07-22 13:28:44 -0400

    UART working and testing with 'f's

--- 412_FinalProject_RFID/src/main.c
+++ 412_FinalProject_RFID/src/main.c
@@ -6,20 +6,22 @@
 //#include <util/delay.h>
 #include "usart_mega.h"
 
-//// UART CONST ////
-#define FOSC 15974400 // Clock Speed
-#define BAUD 9600
-#define MYUBRR FOSC/16/BAUD-1
 
-//// SPI CONST ////
+////////////        UART CONST        ////////////
+#define F_CPU 16000000 // Clock Speed
+#define BAUD 9600
+//#define MYUBRR F_CPU / (9600 * 16UL) - 1 
+#define MYUBRR 0x67
+
+////////////        SPI  CONST        ////////////
 #define SPI_DDR DDRB
-//#define SS		DDB2
+//#define SS	DDB2
 //#define SPCR	SPCR0
 //#define SPDR	SPDR0
 //#define SPSR	SPSR0
 
 void Setup(void);
-void USART_Init(unsigned int);
+void USART_Init(void);
 void USART_Transmit(unsigned char data);
 void USART_PutS(const char *str);
 unsigned char USART_Receive(void);
@@ -29,38 +31,31 @@ char SPI_MasterReceive(void);
 
 int main (void)
 {
-	cli();
+	//cli();
 	Setup();
-	sei();
+	//sei();
 	
 	while(1)
 	{
-		//USART_Transmit(SPI_MasterReceive());
-		
 		USART_Transmit('F');
-		
-		//SPI_MasterTransmit(0xff);
 	};
 }
 
-
 void Setup()
 {
-	// INIT CLOCK
-	sysclk_init();
-	// INIT BOARD
-	board_init();	// INIT USART	USART_Init(MYUBRR);	// INIT SPI
-	SPI_MasterInit();
+	// INIT clock, board, USART, and SPI
+	//sysclk_init();
+	//board_init();	USART_Init();	//SPI_MasterInit();
 }
-
 
 //// USART ////
 
-void USART_Init(unsigned int ubrr)
-{
+void USART_Init()
+{  
 	// Set baud rate
-	UBRR0H = (unsigned char)(ubrr>>8);
-	UBRR0L = (unsigned char)ubrr;
+	UBRR0H = (MYUBRR >> 8);
+	UBRR0L = MYUBRR;
+
 	// Enable receiver and transmitter
 	UCSR0B = (1 << RXEN0) | (1 << TXEN0);
 	// Set frame format: 8data, 2stop bit
@@ -68,16 +63,16 @@ void USART_Init(unsigned int ubrr)
 }void USART_Transmit(unsigned char data)
 {
 	/* Wait for empty transmit buffer */
-	while (!(UCSR0A & (1 << UDRE0))){};
+	while (!(UCSR0A & (1 << UDRE0)));
 	/* Put data into buffer, sends the data */
 	UDR0 = data;
 }unsigned char USART_Receive(void)
 {
 	/* Wait for data to be received */
-	while (!(UCSR0A & (1 << RXC0))){};
+	while (!(UCSR0A & (1 << RXC0)));
 	/* Get and return received data from buffer */
 	return UDR0;
-}void USART_PutS(const char *str){	while(*str){		USART_Transmit(*str);	}}//// SPI ////
+}void USART_PutS(const char *str){	while(*str){		USART_Transmit(*str++);	}}//// SPI ////
 void SPI_MasterInit()
 {
 	// Set MOSI and SCK output, all others input
